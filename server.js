2d494cffc26225dbc3d503bd4d52e314739f01eb
Easton Gorishek <ieaston26@gmail.com>
2018-12-11 16:27:31 -0800

    server, router, and handlers are wired up

--- server.js
+++ server.js
@@ -1,19 +1,19 @@
 const http = require('http');
 const url = require('url');
 
-function start(route) {
+function start(route, handle) {
   function onRequest(req, res) {
     const pathname = url.parse(req.url).pathname;
     console.log('Request for ' + pathname + ' received.');
 
-    route(pathname);
+    route(handle, pathname);
     
     res.writeHead(200, {'Content-Type': 'text/plain'});
     res.write('Hello world');
     res.end();
   }
   http.createServer(onRequest).listen(8888);
-  console.log('Server has started');
+  console.log('Server is jammin.');
 }
 
 exports.start = start;
